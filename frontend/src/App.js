deaad46472cb21dce2263b6bfe0ca093fc08aa2f
KARTHIKNAIR U <96580964+Karthikgraw@users.noreply.github.com>
2024-12-20 11:27:00 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
-// Theme Provider
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { baselightTheme } from './theme/DefaultColors';
-// Router Provider
-import { RouterProvider, useRoutes } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import Router from './routes/Router';
-
-// Redux Provider
 import { Provider } from 'react-redux';
 import store from './store';
-// Tostify
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  // const routing = useRoutes(Router);
   const theme = baselightTheme;
   return (
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <ToastContainer />
         <CssBaseline />
-        {/* {routing} */}
         <RouterProvider router={Router} />
       </Provider>
     </ThemeProvider>

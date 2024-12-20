832091eca08949522ef7426a2a95a428c6f4174e
KARTHIKNAIR U <96580964+Karthikgraw@users.noreply.github.com>
2024-12-20 10:27:57 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // Enable CORS (if frontend and backend are deployed separately)
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: "https://examai-main-frontend.onrender.com",
     credentials: true,
   })
 );

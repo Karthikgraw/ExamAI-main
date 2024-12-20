9e0b09b5c04c7ca030f2058c5cfc750ef43bee8d
KARTHIKNAIR U <96580964+Karthikgraw@users.noreply.github.com>
2024-12-20 11:25:06 +0530

    PrivateRoute.jsx

--- frontend/src/views/authentication/PrivateRoute.jsx
+++ frontend/src/views/authentication/PrivateRoute.jsx
@@ -1,11 +1,11 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
-  if (!userInfo) {
-  }
 
+  // If userInfo is not available, redirect to login
   return userInfo ? <Outlet /> : <Navigate to="/auth/login" replace />;
 };
+
 export default PrivateRoute;

60b74de79f2b520c36fa020ae49e8f9c221c61dc
KARTHIKNAIR U <96580964+Karthikgraw@users.noreply.github.com>
2024-12-20 10:58:20 +0530

    index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Suspense>
-    <>
+    <BrowserRouter> {/* Wrap App with BrowserRouter */}
       <App />
-    </>
+    </BrowserRouter>
   </Suspense>,
 );

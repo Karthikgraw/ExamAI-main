731d5890276640ad5e94ddef57b07e3093b942a3
KARTHIKNAIR U <96580964+Karthikgraw@users.noreply.github.com>
2024-12-20 10:08:31 +0530

    apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,10 +1,18 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://examai-main-backend.onrender.com' });
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.REACT_APP_API_URL, // Use environment variable
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ['User'],
-  // it acts as a parent for other APIs
   endpoints: (builder) => ({}),
 });
